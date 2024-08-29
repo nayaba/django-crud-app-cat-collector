97f3b5be9722c4a061f299867bc6e2b6421cf6e9
Nabila Ayaba <nabilaayaba@gmail.com>
2024-08-29 11:03:24 +0300

    Add static files and nav css

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the CatCollector</h1>')
+    return render(request, 'about.html')
