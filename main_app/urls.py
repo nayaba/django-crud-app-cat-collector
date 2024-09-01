00069342aa1b9f69b1deb2ce26573aa90db2b096
Nabila Ayaba <nabilaayaba@gmail.com>
2024-09-01 10:05:06 +0300

    Create new pages using Django's built in Class-based Views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cat_index, name='cat-index')
+    path('cats/', views.cat_index, name='cat-index'),
+    path('cats/<int:cat_id>', views.cat_detail, name='cat-detail' ),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name="cat-update"),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name="cat-delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
-
-# Fake Cat Model
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -23,4 +10,22 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
